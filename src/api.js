dcb0514929b0faee95d79239a9b1a3ae4b7edd92
lbejiuk <sergeyzarva@gmail.com>
2021-03-06 20:25:29 +0100

    add BroadcastChannel

--- src/api.js
+++ src/api.js
@@ -1,21 +1,12 @@
 const API_KEY = localStorage.getItem("token");
 const AGGREGATE_INDEX = 5;
-
-const tickersHandlers = new Map();
-
-const socket = new WebSocket(
-  `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
-);
-socket.addEventListener("message", e => {
-  const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = JSON.parse(
-    e.data
-  );
-  if (type != AGGREGATE_INDEX || newPrice === undefined) {
-    return;
-  }
-  const handlers = tickersHandlers.get(currency) ?? [];
-  handlers.forEach(fn => fn(newPrice));
-});
+const TYPES = {
+  RUNNING: 1,
+  REQUEST: 2,
+  DATA: 3
+};
+let CONFIGURED = false;
+let socket = null;
 
 function subsctibeToTickerOnWs(ticker) {
   sendMessage({
@@ -23,14 +14,12 @@ function subsctibeToTickerOnWs(ticker) {
     subs: [`5~CCCAGG~${ticker}~USD`]
   });
 }
-
 function unsubsctibeToTickerFromWs(ticker) {
   sendMessage({
     action: "SubRemove",
     subs: [`5~CCCAGG~${ticker}~USD`]
   });
 }
-
 function sendMessage(message) {
   const stringifiedMessage = JSON.stringify(message);
   if (socket.readyState === WebSocket.OPEN) {
@@ -44,6 +33,45 @@ function sendMessage(message) {
     },
     { once: true }
   );
+}
+
+const tickersHandlers = new Map();
+
+var bc = new BroadcastChannel("tabs");
+bc.addEventListener("message", function(message) {
+  if (message.data.type === TYPES.REQUEST) {
+    bc.postMessage({ typ: TYPES.RUNNING });
+  } else if (message.data.type === TYPES.RUNNING) {
+    CONFIGURED = true;
+  } else if (message.data.type === TYPES.DATA) {
+    handleResponse(message.data);
+  }
+});
+
+bc.postMessage({ type: TYPES.REQUEST });
+
+setTimeout(function() {
+  if (CONFIGURED === false) {
+    CONFIGURED = true;
+    socket = new WebSocket(
+      `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
+    );
+    socket.addEventListener("message", e => {
+      bc.postMessage({ type: TYPES.DATA, data: e.data });
+      handleResponse(e);
+    });
+  }
+}, 1);
+
+function handleResponse(e) {
+  const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = JSON.parse(
+    e.data
+  );
+  if (type != AGGREGATE_INDEX || newPrice === undefined) {
+    return;
+  }
+  const handlers = tickersHandlers.get(currency) ?? [];
+  handlers.forEach(fn => fn(newPrice));
 }
 
 export const loadAllCoins = () =>
@@ -60,9 +88,4 @@ export const subscribeToTicker = (ticker, cb) => {
 export const unsubscribeFromTicker = ticker => {
   tickersHandlers.delete(ticker);
   unsubsctibeToTickerFromWs(ticker);
-  //   const subscribers = tickersHandlers.get(ticker) || [];
-  //   tickersHandlers.set(
-  //     ticker,
-  //     subscribers.filter(fn => fn !== cb)
-  //   );
 };
