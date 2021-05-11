79be8357c5c047f5e9cef83ae3c34e0e4f1d5d8f
lbejiuk <sergeyzarva@gmail.com>
2021-05-11 21:23:03 +0200

    change emits

--- src/components/AddButton.vue
+++ src/components/AddButton.vue
@@ -2,7 +2,7 @@
   <button
     :disabled="disabled"
     type="button"
-    @click="click"
+    @click="$emit('click')"
     class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
     :class="{
       'opacity-50': disabled
@@ -36,11 +36,6 @@ export default {
   },
   emits: {
     click: null
-  },
-  methods: {
-    click() {
-      this.$emit("click");
-    }
   }
 };
 </script>
